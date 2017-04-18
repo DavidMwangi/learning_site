2475ab2362c74df826b1f9e640c9c8589aaf3628
DavidMwangi <david.mathenge98@gmail.com>
2017-04-18 14:18:40 +0300

    Django Admin

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello_world/$', views.hello_world),
-    url(r'^$', include('courses.urls')),
+    url(r'^', include('courses.urls')),
 ]
