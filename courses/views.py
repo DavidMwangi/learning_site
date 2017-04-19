dd5ef8c04e743349300fc53a9a7afc833470f3b6
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-04-19 16:42:37 +0300

    Detail View, Not working

--- courses/views.py
+++ courses/views.py
@@ -9,3 +9,9 @@ def course_list(request):
 	courses = Course.objects.all()
 
 	return render(request, 'courses/course_list.html',{'courses':courses})
+
+def course_detail(request, pk):
+
+	course = Course.objects.get(pk=pk)
+
+	return render(request, 'courses/course_detail.html', {'courses':course})
