16e18d986640195e4146a42549479b4d70974024
DavidMwangi <david.mathenge98@gmail.com>
2017-04-20 13:16:14 +0300

    ordering and 404

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Course
 
 # Create your views here.
@@ -11,6 +11,6 @@ def course_list(request):
 
 def course_detail(request, pk):
 
-	course = Course.objects.get(pk=pk)
+	course = get_object_or_404(Course, pk=pk)
 
 	return render(request, 'courses/course_detail.html', {'course':course})
